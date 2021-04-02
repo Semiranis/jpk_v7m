5f90e7e7ed15d7dc6b5d99490920fa5957f13672
Marcin <marcin.lewicz@lewicz.online>
2021-04-02 21:49:03 +0200

    feat: app initiation

---
+++ jpk_v7m_stub/jpk_v7m_stub/doctype/jpk_v7m_generator/jpk_v7m_generator.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, Marcin Lewicz and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('JPK_V7M Generator', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ jpk_v7m_stub/jpk_v7m_stub/doctype/jpk_v7m_generator/test_jpk_v7m_generator.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2021, Marcin Lewicz and Contributors
+# See license.txt
+from __future__ import unicode_literals
+
+# import frappe
+import unittest
+
+class TestJPK_V7MGenerator(unittest.TestCase):
+	pass
